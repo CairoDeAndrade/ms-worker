6180259def91d3cb06593308bfc9ff3ea9a4311d
cairo <cairo.andrade777@gmail.com>
2023-10-31 15:46:29 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/cairo/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/cairo/hrworker/controllers/WorkerController.java
@@ -2,7 +2,10 @@ package com.cairo.hrworker.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import com.cairo.hrworker.services.WorkerService;
 @RequestMapping("/workers")
 public class WorkerController {
 	
+	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private WorkerService service;
 	
@@ -26,6 +34,7 @@ public class WorkerController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		logger.info("PORT = " + env.getProperty("local.server.port"));
 		return ResponseEntity.ok(service.findById(id));
 	}
 }
